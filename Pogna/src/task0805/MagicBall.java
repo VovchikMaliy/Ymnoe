658a78d369d7978cf9d4b379899d5cead057057c
Shisa Ustarkhanov <ustarkhanov.js@gmail.com>
2023-10-24 00:56:06 +0400

    ya krutoy

---
+++ Pogna/src/task0805/MagicBall.java
@@ -0,0 +1,17 @@
+package task0805;
+
+public class MagicBall {
+    private static final String CERTAIN = "Бесспорно";
+    private static final String DEFINITELY = "Определённо да";
+    private static final String MOST_LIKELY = "Вероятнее всего";
+    private static final String OUTLOOK_GOOD = "Хорошие перспективы";
+    private static final String ASK_AGAIN_LATER = "Спроси позже";
+    private static final String TRY_AGAIN = "Попробуй снова";
+    private static final String NO = "Мой ответ — нет";
+    private static final String VERY_DOUBTFUL = "Весьма сомнительно";
+
+    public static String getPrediction() {
+        //напишите тут ваш код
+        return null;
+    }
+}
